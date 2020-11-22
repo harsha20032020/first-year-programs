1939a14b59ed1c452f573201714dba02216b929d
harsha20032020 <nemani.vardhan@students.iiit.ac.in>
2020-11-22 23:53:52 +0530

    Added 22 Nov

--- w3codes loops/4.c
+++ w3codes loops/4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int i,sum=0,n;
-    
-    for (i=1;i<=10;i++)
+    int i, sum = 0, n;
+
+    for (i = 1; i <= 10; i++)
     {
         printf("ENTER THE NUMBER N\n");
-    scanf("%d",&n);
-        sum+=i;
+        scanf("%d", &n);
+        sum += n;
     }
-    printf("the sum = %d\n",sum);
+    printf("the sum = %d\n", sum);
 }
