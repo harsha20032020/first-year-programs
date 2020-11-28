506200a5c422d41762342db7a1b438d1de06133c
harsha20032020 <nemani.vardhan@students.iiit.ac.in>
2020-11-28 13:53:03 +0530

    Added Nov 28

---
+++ w3codes/122.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,m,sum=0;
+    printf("ENTER THE NUMBER\n");
+    scanf("%d",&n);
+    printf("ENTER THE NUMBER OF TERMS\n");
+    scanf("%d",&m);
+    for (i=n;i<n+2*m;i+=2)
+    {
+        if (i%2==0)
+        {
+            sum+=i;
+        }
+    }
+    printf("%d is the sum\n",sum);
+}
